a04d7e17df7e71c711b57df5bbf410575fb3243e
Jan Rosa <jan.rosa02@gmail.com>
2026-02-16 19:22:47 +0100

    fixed texts

--- texts.py
+++ texts.py
@@ -3,17 +3,17 @@ from typing import Iterable, Iterator
 
 
 kapitan_bomba_cytaty = [
-    # "Tępy chuju, masz mnie za idiotę? Wylądujemy w nocy!",
-    # "Średnia hawajska dla każdego!",
-    # "Moja dupa jest ciasna jak po praniu, a twoja matka to chuj.",
-    # "Nie ważne co mam w sercu, ważne co mam w dupie.",
-    # "Umysł robota walczy z umysłem debila.",
-    # "Zrobię ci z dupy kropkę nad i!",
-    # "Pachną nigdy niemyty siusiakiem, tak jak chorąży Torpeda.",
-    # "Może i robimy chujowo, ale kto robi dobrze?",
-    # "Domino - leć po kieliszki, Grzegorz - smaruj stoły gównem!",
-    # "Ryby w Morskim Oku zdechły. A w Czarnym Stawie... zdechły.",
-    # "Jeżeli chodzi o dziewczynki, to można powiedzieć, że jestem jaroszem.",
+    "Tępy chuju, masz mnie za idiotę? Wylądujemy w nocy!",
+    "Średnia hawajska dla każdego!",
+    "Moja dupa jest ciasna jak po praniu, a twoja matka to chuj.",
+    "Nie ważne co mam w sercu, ważne co mam w dupie.",
+    "Umysł robota walczy z umysłem debila.",
+    "Zrobię ci z dupy kropkę nad i!",
+    "Pachną nigdy niemyty siusiakiem, tak jak chorąży Torpeda.",
+    "Może i robimy chujowo, ale kto robi dobrze?",
+    "Domino - leć po kieliszki, Grzegorz - smaruj stoły gównem!",
+    "Ryby w Morskim Oku zdechły. A w Czarnym Stawie... zdechły.",
+    "Jeżeli chodzi o dziewczynki, to można powiedzieć, że jestem jaroszem.",
     "Panie kapitanie, a co by pan zrobił, gdyby się pan dowiedział, \nże pana siostra opierdala kiełbachy kosmitom?",
     "Donna mamma es chujoczita",
     "Matka była mi jak brat, a jak zabijesz brata to jakbyś zabił matkę"
